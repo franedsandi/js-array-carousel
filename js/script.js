06ab84f9d3636fc38f21e71e780576f32ddf2eea
franedsandi <franedsandi@gmail.com>
2023-09-13 15:40:35 +0200

    [feat] display first image

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 /* 
-1. with a citle, print the pictures inside items-wrapper
+1. with a cicle, print the pictures inside items-wrapper
 2. pick the elements with the class "item" and safe each in array
 3. take out the hide class on the first element
 4. when next ( arrow down )
@@ -12,3 +12,28 @@
 **extra bonus**
 8. add the thumbnails at the left of the image in css using active as previus homeworks
 9.make each picture active when the picture is in use*/
+
+/* elements */
+const itemsWrapper = document.querySelector('.items-wrapper');
+const btnBottom = document.querySelector('.bottom');
+const btnTop = document.querySelector('.top');
+
+const images = [
+  './img/01.webp',
+  './img/02.webp',
+  './img/03.webp',
+  './img/04.webp',
+  './img/05.webp'];
+  
+let counterImg = 0;
+itemsWrapper.innerHTML = '';
+
+//1.
+for(let i = 0; i < images.length; i++){
+  const image = images[i];
+  itemsWrapper.innerHTML += '<img src="' + images[i] +  '" class="item hide">';
+}
+//2.
+const itemsCollection = document.getElementsByClassName('item');
+//3. 
+itemsCollection [counterImg].classList.remove('hide');
